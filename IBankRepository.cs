406b1565eeb9d5fc0aaac7ac357066f311ae95fa
Alexandru Bulat <alexandru.bulat@aspira.software>
2022-10-17 09:41:47 +0300

    Init

---
+++ IBankRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bnmmoney
+{
+    public interface IBankRepository
+    {
+       Task<List<Valute>> getData();
+    }
+}
---
+++ ValCurs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace bnmmoney
+{
+
+    [XmlRoot(ElementName = "ValCurs")]
+    public class ValCurs
+    {
+        [XmlElement(ElementName = "Valute")]
+        public List<Valute> Valute { get; set; }
+
+    }
+}
