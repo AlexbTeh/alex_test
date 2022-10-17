cd4581d4e5ca83296077f2a78a5900ee06e3695c
Alexandru Bulat <alexandru.bulat@aspira.software>
2022-10-17 10:38:01 +0300

    Delete cached files

---
+++ BankRepositiry.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using System.Xml.Serialization;
+
+namespace bnmmoney
+{
+    public class BankRepository : IBankRepository
+    {
+        //
+        public async Task<List<Valute>> getData()
+        {
+            var data = DateTime.Now.ToString("dd.MM.yyyy"); ;
+            var url = $"https://bnm.md/ro/official_exchange_rates?get_xml=1&date={data}";
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+            var content = client.GetStreamAsync(url).Result;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ValCurs));
+            var valsCurs = (ValCurs)serializer.Deserialize(content);
+
+            Console.WriteLine("Count massive" + valsCurs.Valute.Count);
+
+            return valsCurs.Valute;
+        }
+    }
+}
